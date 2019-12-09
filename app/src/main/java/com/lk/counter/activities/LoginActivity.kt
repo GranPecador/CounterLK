148671b7f36cf1a7c0efbf948a28d25d01f75fc7
lennuz17 <lennuz17@gmail.com>
2019-12-10 00:21:12 +0300

    added Material design

--- app/src/main/java/com/lk/counter/activities/LoginActivity.kt
+++ app/src/main/java/com/lk/counter/activities/LoginActivity.kt
@@ -2,6 +2,8 @@ package com.lk.counter.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
 import com.lk.counter.R
 
 class LoginActivity : AppCompatActivity() {
@@ -9,5 +11,10 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginButton = findViewById<MaterialButton>(R.id.login_enter_btn)
+        loginButton.setOnClickListener{
+            Toast.makeText(this, "pfq", Toast.LENGTH_LONG).show()
+        }
     }
 }
